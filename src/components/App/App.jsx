b04f3e41f673903a0297b16bb1eca268198d6f38
CodyHagberg <codyhagberg1117@gmail.com>
2025-12-04 14:39:23 -0700

    Main Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Header from '../Header/Header.jsx' 
+import Main from '../Main/Main.jsx'
 
 function App() {
 
   return (
-    <div className="page"><div className="page__content">
-    <Header/>
-    </div>
+    <div className="page">
+      <div className="page__content">
+        <Header/>
+        <Main/>
+      </div>
     </div>
   )
 }
