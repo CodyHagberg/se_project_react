614e8480fcc8116381d55e025ebb1e26c010af23
CodyHagberg <codyhagberg1117@gmail.com>
2025-12-05 17:28:51 -0700

    Weather Card

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,14 @@
+import WeatherCard from '../WeatherCard/WeatherCard.jsx';
+
 function Main() {
     return (
-        <>
-        </>
+        <main>
+            <WeatherCard />
+            <section className ="cards">
+                <p className="cards__text">Today is 75&deg; F / You may want to wear:</p>
+                /.* Add clothing suggestions here */
+            </section>
+        </main>
     )}
 
 export default Main;
