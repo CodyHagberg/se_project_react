2c1b975b38e6fc0c9f9682a43597a25cd69524ed
CodyHagberg <codyhagberg1117@gmail.com>
2025-12-06 09:45:42 -0700

    WeatherCard adjustments

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,7 +1,11 @@
+import "./WeatherCard.css"
+import sunny from "../../assets/Sunny.svg"
+
 function WeatherCard() {
 return <>
 <section className ="weather-card">
     <p className="weather-card__temp">75 &deg; F</p>
+    <img src={sunny} alt="" className="weather-card__image"/>
 </section>
 </>
 }
