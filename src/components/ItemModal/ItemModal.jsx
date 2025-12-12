1873ce9642ef7934d477e1486da9c7f00e71a783
CodyHagberg <codyhagberg1117@gmail.com>
2025-12-11 17:45:37 -0700

    reworked

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,11 +1,11 @@
 import "./ItemModal.css"
-import closeIcon from "../../assets/close_icon.png";
+import closeIcon from "../../assets/white-close.png";
 
 function ItemModal({activeModal, handleCloseClick, card}) {
 return(
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
         <div className="modal__content modal__content_type_image">
-             <button onClick={handleCloseClick} className="modal__close" type="button"><img src={closeIcon} alt="Close" className="modal__close-icon" />
+             <button onClick={handleCloseClick} className="modal__close" type="button"><img src={closeIcon} alt="Close" className="modal__close-icon-white" />
         </button>
         <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
